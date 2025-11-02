a9a9b8b0be9a960e9832372b9edb6b6a4d99f821
oyz13085 <oyz13085@gmail.com>
2025-11-02 14:54:33 +0800

    Update SQL.java

--- src/constant/SQL.java
+++ src/constant/SQL.java
@@ -3,7 +3,7 @@ package constant;
 public class SQL {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/storedb";
     public static final String DB_Username = "root";
-    public static final String DB_Password = "OoiZhe9@";
+    public static final String DB_Password = "-";
     public static final String DB_Employee = "employee";
     public static final String DB_Clock = "clock_time";
 
