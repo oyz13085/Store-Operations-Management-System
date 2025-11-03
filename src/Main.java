59a65ed0b8e6a06827539cf67bff3c5d6f19c381
oyz13085 <oyz13085@gmail.com>
2025-11-03 23:36:00 +0800

    Added Stock System

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import constant.Color;
 import java.util.Scanner;
 
 public class Main {
-    static Employee employee = new Employee();
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        boolean isLogin = true;
         boolean running = true;
 
         Employee employee = new Employee();
@@ -24,14 +22,15 @@ public class Main {
             if(employee.checkLogin(userID,password)){
                 System.out.println("\nLogin " + Color.GREEN + "Successful" + Color.RESET + "!\n");
 
-                //if manager -> manager menu, employee -> employee menu
-                while(managerMenu.isLogin){
-                    managerMenu.ManagerFunction(userID);
+                if(Employee.Role.contains("Manager")){
+                    while(ManagerMenu.isLogin){
+                        managerMenu.ManagerFunction(userID);
+                    }
+                }else{
+                    while(EmployeeMenu.isLogin){
+                        employeeMenu.EmployeeFunction(userID);
+                    }
                 }
-                while(employeeMenu.isLogin){
-                    employeeMenu.EmployeeFunction(userID);
-                }
-
             }else {
                 System.out.println("\nLogin" + Color.RED + " Failed: Invalid ID or Password.\n" + Color.RESET);
             }
@@ -39,9 +38,6 @@ public class Main {
 
 
     }
-        //login and logout/registration for employees
-
-        //working hours(clock in and clock out)
 
 }
 
